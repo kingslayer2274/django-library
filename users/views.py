06b0196b72beb8f95636d1e084cd21d98d950975
Ahmed Magdi <amagdi825@gmail.com>
2021-09-18 17:13:36 +0200

    final project

---
+++ users/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render,redirect
+from django.urls import reverse_lazy
+from django.views import generic
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from django.contrib.auth import authenticate, login,logout  
+from django.contrib.auth.models import User
+from .forms import SignupForm,UserLoginForm
+
+# Login page
+def index(request): 
+    login_form = UserLoginForm()
+    context = {'form': login_form}
+    return render(request,"/users/login",context)
+
+# Registeration page
+def register(request):
+    if request.method == "POST":
+        # sava new user to the database
+        form  = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/users/login")
+        else:
+            return form.errors
+    else:
+        # render registraion page
+        form  = SignupForm()
+        context = {"form": form}
+        return render(request, "registration/register.html",context)
+
+
+
